3a583b91b6b55c4301b40ffa3770663910520da8
wzlxjtu <wzlxjtu@gmail.com>
2015-04-02 13:34:29 +0000

    Changed root

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'application#hello2'
+  #root 'application#hello2'
+  root 'members#index'
   
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
