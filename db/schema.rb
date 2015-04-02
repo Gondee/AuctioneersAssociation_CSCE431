7f0c352731c03bdf249227ef4bfeb7760686f56c
wzlxjtu <wzlxjtu@gmail.com>
2015-04-02 05:10:40 +0000

    Members Manual Import

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,39 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150320041848) do
+ActiveRecord::Schema.define(version: 20150402050709) do
+
+  create_table "members", force: :cascade do |t|
+    t.string   "Last_Name"
+    t.string   "First_Name"
+    t.string   "Company"
+    t.integer  "TX_License"
+    t.text     "Street1"
+    t.string   "City"
+    t.string   "State"
+    t.integer  "Zip"
+    t.integer  "Main_Phone"
+    t.string   "Main_Email"
+    t.string   "Customer_Type"
+    t.integer  "Date_Joined_TAA"
+    t.string   "Pymt_Type"
+    t.integer  "Pymt_Amt"
+    t.date     "Pymt_Date"
+    t.text     "Notes"
+    t.boolean  "Added_to_WebBase"
+    t.integer  "PAC_Contribution"
+    t.integer  "Annual_Convention_CE_Hours"
+    t.integer  "Online_CE_Hours"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
+
+  create_table "microposts", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
