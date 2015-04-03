3cfeb2e5c9fdbf09bf50cf9e60fe145da84839b1
wzlxjtu <wzlxjtu@gmail.com>
2015-04-03 03:31:18 +0000

    help

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   
-  root 'static_pages#home'
+  root 'static_pages#help'
   #root 'application#hello2'
   #root 'members#index'
   
