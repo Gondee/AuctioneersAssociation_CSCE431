31d310e4db17e332e211c86ced659053c2b7c2f9
cloverzh <zhouhe727@gmail.com>
2015-04-04 06:28:14 +0000

    change new member link

--- db/schema.rb
+++ db/schema.rb
@@ -38,18 +38,4 @@ ActiveRecord::Schema.define(version: 20150403181057) do
     t.datetime "updated_at",                 null: false
   end
 
-  create_table "microposts", force: :cascade do |t|
-    t.text     "content"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
