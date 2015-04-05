e120cec5ca8df7276372c145bc6d05f04242770c
Josh <joshuackruger@gmail.com>
2015-04-05 15:52:31 +0000

    updateMember_spec increase

--- spec/updateMember_spec.rb
+++ spec/updateMember_spec.rb
@@ -2,20 +2,7 @@ require 'spec_helper'
  
 describe MembersController do
     
-#    describe "Create_Members" do
-#        it "should create a new instance of a user given valid attributes" do
-#        @member = Member.new(Last_Name: "Example_LastName", First_Name: "Example_FirstName", 
-#        Zip:"123456", Main_Phone: "1234567", Main_Email: "test@test.test")
-#        Member.create!(@member.attributes)
-#        end
-#    end
-    
     it "should change a members attributes from the default" do
-        #@newMemberParams = member_params.new(Last_Name:"Last_Name_Updated", First_Name:"First_Name_Updated",Company:"Company_Updated",TX_License:"000000",
-        #Street1:"Street_Updated",City:"City_Updated",State:"State_Upated",Zip:"000000",Main_Phone:"000000000",Main_Email:"email_updated@update.com",Customer_Type:"type_updated",
-        #Date_Joined_TAA:"00",Pymt_Type:"Updated",Notes:"Notes_Updated",Added_to_WebBase:"Webbase_Updated",PAC_Contribution:"PAC_Updated",Annual_Convention_CE_Hours:"00",
-        #Online_CE_Hours:"00")
-        
         @member = Member.new(Last_Name: "Example_LastName", First_Name: "Example_FirstName", 
         Zip:"123456", Main_Phone: "1234567", Main_Email: "test@test.test",Date_Joined_TAA:"2000")
         Member.create!(@member.attributes)
@@ -24,17 +11,20 @@ describe MembersController do
         Street1:"Street_Updated",City:"City_Updated",State:"State_Upated",Zip:"000000",Main_Phone:"000000000",Main_Email:"email_updated@update.com",Customer_Type:"type_updated",
         Date_Joined_TAA:"2000",Pymt_Type:"Updated",Notes:"Notes_Updated",Added_to_WebBase:"Webbase_Updated",PAC_Contribution:"PAC_Updated",Annual_Convention_CE_Hours:"00",
         Online_CE_Hours:"00")
-        #update(id, attributes)
-        #Member.update(@member,Last_Name:"Last_Name_Updated", First_Name:"First_Name_Updated",Company:"Company_Updated",TX_License:"000000",
-        #Street1:"Street_Updated",City:"City_Updated",State:"State_Upated",Zip:"000000")
         end
+    it "should update with only a partial argument list" do 
+        @member = Member.new(Last_Name: "Example_LastName", First_Name: "Example_FirstName", 
+        Zip:"123456", Main_Phone: "1234567", Main_Email: "test@test.test",Date_Joined_TAA:"2000")
+        Member.create!(@member.attributes)
+        @member.save
+        Member.update(@member.id, Last_Name:"Last_Name_Updated", First_Name:"First_Name_Updated",Company:"Company_Updated",TX_License:"000000",
+        Street1:"Street_Updated",City:"City_Updated",State:"State_Upated",Zip:"000000",Main_Phone:"000000000",Main_Email:"email_updated@update.com",Customer_Type:"type_updated",
+        Date_Joined_TAA:"2000",Pymt_Type:"Updated")
+    end
     
 end
-
-# ALL Params
-#Last_Name:"Last_Name_Updated", First_Name:"First_Name_Updated",Company:"Company_Updated",TX_License:"000000",
-#        Street1:"Street_Updated",City:"City_Updated",State:"State_Upated",Zip:"000000",Main_Phone:"000000000",Main_Email:"email_updated@update.com",Customer_Type:"type_updated",
-#        Date_Joined_TAA:"00",Pymt_Type:"Updated",Notes:"Notes_Updated",Added_to_WebBase:"Webbase_Updated",PAC_Contribution:"PAC_Updated",Annual_Convention_CE_Hours:"00",
-#        Online_CE_Hours:"00"
-#
-#
+ #@newMemberParams = member_params.new(Last_Name:"Last_Name_Updated", First_Name:"First_Name_Updated",Company:"Company_Updated",TX_License:"000000",
+        #Street1:"Street_Updated",City:"City_Updated",State:"State_Upated",Zip:"000000",Main_Phone:"000000000",Main_Email:"email_updated@update.com",Customer_Type:"type_updated",
+        #Date_Joined_TAA:"00",Pymt_Type:"Updated",Notes:"Notes_Updated",Added_to_WebBase:"Webbase_Updated",PAC_Contribution:"PAC_Updated",Annual_Convention_CE_Hours:"00",
+        #Online_CE_Hours:"00")
+        
