8f1bcc7ecd5fb596f75bde49f3387e905aebfcb9
wzlxjtu <wzlxjtu@gmail.com>
2015-04-07 03:56:56 +0000

    fix import test

--- spec/models/importMember_spec.rb
+++ spec/models/importMember_spec.rb
@@ -3,20 +3,22 @@ require 'spec_helper'
 describe MembersController, type: :controller do
     describe "merbers import" do
         it "redirects to the members page" do
-            allow(Member).to receive(:import).with("foo.txt")
-            post :import, file: "foo.txt"
+            allow(Member).to receive(:import).with("foo.csv")
+            post :import, file: "foo.csv"
             expect(response).to redirect_to members_url
         end
         
         it "adds a notice" do
-            allow(Member).to receive(:import).with("foo.txt")
-            post :import, file: "foo.txt"
+            allow(Member).to receive(:import).with("foo.csv")
+            post :import, file: "foo.csv"
             expect(flash[:notice]).to eq "Products imported."
         end
         
         it "imports the members file" do
-            expect(Member).to receive(:import).with("foo.txt")
-            post :import, file: "foo.txt"
+            expect(Member).to receive(:import).with("foo.csv")
+            post :import, file: "foo.csv"
         end
+        
+
     end
 end   
