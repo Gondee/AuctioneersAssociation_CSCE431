a3795d37006433bf17da5ac701dd71d0be3c6c78
Josh <joshuackruger@gmail.com>
2015-04-24 16:53:16 +0000

    modified:   .c9/metadata/workspace/app/controllers/payments_controller.rb

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -4,7 +4,7 @@ class PaymentsController < ApplicationController
   # GET /payments
   # GET /payments.json
   def index
-    @payments = Payment.all
+    @payments = Payment.where(member_id: current_user.id )
   end
 
   # GET /payments/1
