d47cca37b1b9d655fc3c1780198a7e3c39e6f336
wzlxjtu <wzlxjtu@gmail.com>
2015-04-27 16:48:24 +0000

    fix-admin-migration

--- db/migrate/20150421072334_add_reset_to_members.rb
+++ db/migrate/20150421072334_add_reset_to_members.rb
@@ -2,6 +2,5 @@ class AddResetToMembers < ActiveRecord::Migration
   def change
     add_column :members, :reset_digest, :string
     add_column :members, :reset_sent_at, :datetime
-    change_column :members, :admin, :integer
   end
 end
