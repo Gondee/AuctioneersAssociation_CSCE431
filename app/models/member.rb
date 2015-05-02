36c317bc94c269624c614d04be20202d81244df2
cloverzh <zhouhe727@gmail.com>
2015-05-02 00:08:44 +0000

    Email validation

--- app/models/member.rb
+++ app/models/member.rb
@@ -21,8 +21,8 @@ class Member < ActiveRecord::Base
    VALID_ZIP_REGEX=/\A[0-9]+\z/
    validates :Zip, length: { minimum: 5 },  format: { with: VALID_ZIP_REGEX }, allow_blank: true
    
-   VALID_PHONE_REGEX=/\A[0-9]+\z/
-   validates :Main_Phone, length: { maximum: 255 },  format: { with: VALID_PHONE_REGEX }
+   #VALID_PHONE_REGEX=/\A[0-9]+\z/
+   #validates :Main_Phone, length: { maximum: 255 },  format: { with: VALID_PHONE_REGEX }
    
    
    
